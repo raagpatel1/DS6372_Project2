9a143f85d7b62c9bd8de4b9c7b1026b8d013fec7
Patel, Raag <raagp@mail.smu.edu>
2022-11-16 20:35:17 -0600

    Create EDA.R

---
+++ EDA.R
@@ -0,0 +1,20 @@
+# Raag Patel setwd, make sure to comment on your copy!
+# setwd("D:/School/GitHub/DS6372_Project2")
+
+##################
+# IF YOU EDIT THIS DOCUMENT, PLEASE MAKE SURE TO COMMENT WHAT YOUR LINES DO 
+#################
+
+# Make sure to throw necessary libraries up here.
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(MASS)
+
+# Load in the data.
+
+library(aplore3)
+
+RawData = glow_bonemed
+
+summary(RawData)
