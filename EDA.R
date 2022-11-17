f05474b3aeddf7aa9e95db34edcbc3600886da16
Patel, Raag <raagp@mail.smu.edu>
2022-11-16 20:45:01 -0600

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -7,6 +7,7 @@
 
 # Make sure to throw necessary libraries up here.
 library(dplyr)
+library(tidyr)
 library(tidyverse)
 library(ggplot2)
 library(MASS)
@@ -17,4 +18,15 @@ library(aplore3)
 
 RawData = glow_bonemed
 
+# The data comes from glow500 with some variables added, and the var desc are in 
+# glow 500. 
+
+?glow500
+?glow_bonemed
+
+# Looking at the data
+head(RawData)
 summary(RawData)
+
+
+
